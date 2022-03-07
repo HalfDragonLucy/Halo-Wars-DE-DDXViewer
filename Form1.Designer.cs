ec35f3d266c1c213f89eb99062688dcae8750601
HalfDragonLucy <halfdragon14@gmail.com>
2022-03-07 20:42:04 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace DDXViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.DisplayBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.themeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ThemesList = new System.Windows.Forms.ToolStripComboBox();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.DisplayBox);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 24);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1584, 837);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // DisplayBox
+            // 
+            this.DisplayBox.BackColor = System.Drawing.Color.Transparent;
+            this.DisplayBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DisplayBox.Location = new System.Drawing.Point(0, 0);
+            this.DisplayBox.Name = "DisplayBox";
+            this.DisplayBox.Size = new System.Drawing.Size(1584, 837);
+            this.DisplayBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.DisplayBox.TabIndex = 0;
+            this.DisplayBox.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.themeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1584, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // themeToolStripMenuItem
+            // 
+            this.themeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ThemesList});
+            this.themeToolStripMenuItem.Name = "themeToolStripMenuItem";
+            this.themeToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.themeToolStripMenuItem.Text = "Themes";
+            // 
+            // ThemesList
+            // 
+            this.ThemesList.FlatStyle = System.Windows.Forms.FlatStyle.Standard;
+            this.ThemesList.Name = "ThemesList";
+            this.ThemesList.Size = new System.Drawing.Size(121, 23);
+            this.ThemesList.Text = "Color";
+            this.ThemesList.SelectedIndexChanged += new System.EventHandler(this.ThemesList_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Halo Wars DE: DDXViewer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.MainPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.PictureBox DisplayBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem themeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox ThemesList;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using DDXViewer.Properties;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace DDXViewer
+{
+    public partial class Form1 : Form
+    {
+        private static readonly string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        private static readonly string exeDirectory = Path.GetDirectoryName(exePath);
+        private static readonly string resourcesPath = $@"{exeDirectory}\Ressources";
+        private static readonly string toolsPath = $@"{exeDirectory}\Tools";
+        private static readonly string texConvPath = $@"{toolsPath}\texconv.exe";
+        private static string resourceFilePath;
+        private static string resourceFileConvertedPath;
+        private int numberOfFiles;
+        private Image displayImage;
+
+        private static readonly ProcessStartInfo TexConvInfo = new ProcessStartInfo
+        {
+            UseShellExecute = true,
+            FileName = texConvPath,
+            WindowStyle = ProcessWindowStyle.Hidden
+        };
+        private static readonly Process TexConv = new Process { StartInfo = TexConvInfo, EnableRaisingEvents = true };
+
+        private readonly string[] arguments = Environment.GetCommandLineArgs();
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Hide the Form until the picture is loaded
+            Hide();
+            InitializeThemes();
+
+
+            // Might need to change that later
+            if (!InputIsDDX())
+            {
+                Application.Exit();
+            }
+
+            // Check if TexConv and Ressources folder exists, if not, create them.
+            if (!CheckIntegrity())
+            {
+                CreateIntegrity();
+            }
+
+            // Get The numbers of files in the ressoucers to generate names
+            GetInputedFiles();
+
+            // Copy the picture into Ressources folder
+            CopyOriginalDDX();
+
+            // Convert the DDX into a PNG 
+            ConvertRessourceDDX();
+
+            // Display the Converted DDX into the form
+            DisplayConvertedDDX();
+
+        }
+
+        private void InitializeThemes()
+        {
+            Type colorType = typeof(System.Drawing.Color);
+            PropertyInfo[] propInfos = colorType.GetProperties(BindingFlags.Static | BindingFlags.DeclaredOnly | BindingFlags.Public);
+            foreach (PropertyInfo propInfo in propInfos)
+            {
+                if (propInfo.Name != "Transparent")
+                {
+                    ThemesList.Items.Add(propInfo.Name);
+                }
+            }
+
+            if (Settings.Default.Color != "")
+            {
+                BackColor = Color.FromName(Settings.Default.Color);
+            }
+        }
+
+        private void GetInputedFiles()
+        {
+            DirectoryInfo info = new DirectoryInfo(resourcesPath);
+            if (info.GetFiles().Length == 0)
+            {
+                numberOfFiles = 1;
+            }
+            else
+            {
+                numberOfFiles = info.GetFiles().Length + 1;
+            }
+        }
+
+        private bool InputIsDDX()
+        {
+            if (Path.GetExtension(arguments[1]) == ".ddx")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CleanUp();
+        }
+
+        private bool CheckIntegrity()
+        {
+            if (Directory.Exists(resourcesPath) && Directory.Exists(toolsPath))
+            {
+                if (File.Exists(texConvPath))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void CreateIntegrity()
+        {
+            if (!Directory.Exists(resourcesPath)) { Directory.CreateDirectory(resourcesPath); }
+            if (!Directory.Exists(toolsPath)) { Directory.CreateDirectory(toolsPath); }
+            if (!File.Exists(texConvPath))
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile("https://github.com/microsoft/DirectXTex/releases/download/feb2022/texconv.exe", texConvPath);
+                }
+            }
+
+        }
+
+        private void CopyOriginalDDX()
+        {
+            resourceFilePath = $@"{resourcesPath}\{numberOfFiles}.dds";
+            File.Copy(arguments[1], resourceFilePath, true);
+        }
+
+        private void ConvertRessourceDDX()
+        {
+            TexConv.StartInfo.Arguments = $"\"{resourceFilePath}\" -ft png -y -o \"{resourcesPath}\"";
+            TexConv.Start();
+
+            TexConv.WaitForExit();
+
+            if (File.Exists(resourceFilePath)) { File.Delete(resourceFilePath); }
+            resourceFileConvertedPath = $@"{resourcesPath}\{numberOfFiles}.png";
+        }
+
+        private void DisplayConvertedDDX()
+        {
+            displayImage = Image.FromFile(resourceFileConvertedPath);
+            DisplayBox.Image = displayImage;
+
+            Width = displayImage.Width;
+            Height = displayImage.Height;
+        }
+
+        private void CleanUp()
+        {
+            Settings.Default.Save();
+            displayImage?.Dispose();
+            DisplayBox.Image?.Dispose();
+            if (File.Exists(resourceFileConvertedPath)) { File.Delete(resourceFileConvertedPath); }
+        }
+
+        private void ThemesList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BackColor = Color.FromName(ThemesList.SelectedItem.ToString());
+            Settings.Default.Color = ThemesList.SelectedItem.ToString();
+        }
+    }
+}
+
